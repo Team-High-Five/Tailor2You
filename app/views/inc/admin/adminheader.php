006534044c1b23fb280966c7d3d6e984dc14afd2
Thariniii <2022is016@stu.ucsc.cmb.ac.lk>
2024-12-01 18:29:40 +0530

    admin login

--- app/views/inc/admin/adminheader.php
+++ app/views/inc/admin/adminheader.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title><?php echo SITENAME ?></title>
@@ -8,21 +9,31 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="<?php echo URLROOT; ?>/public/js/admin.js" defer></script>
 </head>
+
 <body>
     <div class="background-overlay"></div>
     <!-- Header Section -->
     <header class="header">
         <div class="logo">
-            <img src="<?php echo URLROOT; ?>/public/img/admin/logo.jpg" alt="Tailor2You Logo"> <!-- Replace with your logo image path -->
+            <img src="<?php echo URLROOT; ?>/public/img/admin/logo.jpg" alt="Tailor2You Logo">
+            <!-- Replace with your logo image path -->
         </div>
         <div class="profile-section">
             <i class="fas fa-bell"></i>
-            <div class="profile-info">
-                <img src="<?php echo URLROOT; ?>/public/img/admin/propic.png" alt="Profile Picture" class="profile-pic">
-                <div>
-                    <p class="profile-title">Title</p>
-                    <p class="profile-description">Description</p>
-                </div>
+            <div class="user-info">
+                <span>
+                    <?php if (isset($_SESSION['user_first_name'])): ?>
+                        <?php echo $_SESSION['user_first_name']; ?>
+                    <?php else: ?>
+                        Guest
+                    <?php endif; ?>
+                </span>
+                <?php if (!empty($_SESSION['user_profile_pic'])): ?>
+                    <img src="data:image/jpeg;base64,<?php echo base64_encode($_SESSION['user_profile_pic']); ?>"
+                        alt="User Avatar">
+                <?php else: ?>
+                    <img src="<?php echo URLROOT; ?>/public/img/Avatar.png" alt="User Avatar">
+                <?php endif; ?>
             </div>
             <i class="fas fa-ellipsis-h" onclick="toggleDropdown()"></i>
             <!-- Dropdown menu -->
@@ -33,4 +44,5 @@
         </div>
     </header>
 </body>
+
 </html>
