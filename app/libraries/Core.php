63d9f038a5805c3c80e430705f2b09c69d6a63fa
Bavindu Shamen <140163711+bavindu122@users.noreply.github.com>
2025-01-29 11:39:36 +0530

    Implement custom 404 error handling and improve URL routing; refactor Core and Pages controllers

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -1,42 +1,69 @@
 <?php
 
-class Core{
-    //ur format : controller/method/params
+class Core
+{
+    // URL format: controller/method/params
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
     protected $params = [];
 
-    public function __construct(){
+    public function __construct()
+    {
         $url = $this->getUrl();
-        if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+
+        // Check if the controller exists
+        if (isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
             $this->currentController = ucwords($url[0]);
             unset($url[0]);
-            //call back the controller
-            require_once '../app/controllers/' . $this->currentController . '.php';
-            $this->currentController = new $this->currentController;
-
-            //check for the method
-            if(isset($url[1])){
-                if(method_exists($this->currentController, $url[1])){
-                    $this->currentMethod = $url[1];
-                    unset($url[1]);
-                }
-            }
-            //get params
-            $this->params = $url ? array_values($url) : [];
-
-            //call a method with array of params
-            call_user_func_array([$this->currentController, $this->currentMethod], $this->params);     
+        } else {
+            // Controller does not exist, load 404 page
+            $this->currentController = 'Pages';
+            $this->currentMethod = 'notFound';
+            $this->params = [];
+            $this->loadController();
+            return;
         }
 
+        // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
+        $this->currentController = new $this->currentController;
+
+        // Check if the method exists
+        if (isset($url[1])) {
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                unset($url[1]);
+            } else {
+                // Method does not exist, load 404 page
+                $this->currentController = 'Pages';
+                $this->currentMethod = 'notFound';
+                $this->params = [];
+                $this->loadController();
+                return;
+            }
+        }
+
+        // Get params
+        $this->params = $url ? array_values($url) : [];
+
+        // Call a method with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
-    public function getUrl(){
-        if(isset($_GET['url'])){
-           $url = rtrim($_GET['url'], '/');
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
             return $url;
         }
+    }
+
+    private function loadController()
+    {
+        require_once '../app/controllers/' . $this->currentController . '.php';
+        $this->currentController = new $this->currentController;
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 }
