96f7a29c9f9f30491cb1939622b48f741dd9fe47
Bavindu Shamen <140163711+bavindu122@users.noreply.github.com>
2025-02-12 17:42:38 +0530

    Refactor tailor page: update data retrieval to use getAllTailors, enhance profile card display, and improve CSS for responsiveness

--- app/models/M_Pages.php
+++ app/models/M_Pages.php
@@ -1,14 +1,22 @@
 <?php
 
-class M_Pages{
+class M_Pages
+{
     private $db;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->db = new Database;
     }
 
-    public function getUsers(){
+    public function getUsers()
+    {
         $this->db->query("SELECT * FROM users");
+        return $this->db->resultSet();
+    }
+    public function getAllTailors()
+    {
+        $this->db->query("SELECT user_id, CONCAT(first_name, ' ', last_name) AS name, bio, profile_pic FROM users WHERE user_type = 'tailor'");
         return $this->db->resultSet();
     }
 }
