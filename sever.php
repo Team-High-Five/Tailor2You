962da9a14390b3deece940f91a97b6d77ff4b38f
Thariniii <2022is016@stu.ucsc.cmb.ac.lk>
2025-02-20 12:12:31 -0300

    edit message view

---
+++ sever.php
@@ -0,0 +1,52 @@
+<?php
+// Include Ratchet and its dependencies
+require __DIR__ . '/libs/ratchet/vendor/autoload.php';
+require __DIR__ . '/libs/guzzlehttp/psr7/src/functions_include.php';
+require __DIR__ . '/libs/symfony/http-foundation/autoload.php';
+require __DIR__ . '/libs/symfony/routing/autoload.php';
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        foreach ($this->clients as $client) {
+            if ($from !== $client) {
+                $client->send($msg);
+            }
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        $conn->close();
+    }
+}
+
+use Ratchet\Server\IoServer;
+use Ratchet\Http\HttpServer;
+use Ratchet\WebSocket\WsServer;
+
+$server = IoServer::factory(
+    new HttpServer(
+        new WsServer(
+            new Chat()
+        )
+    ),
+    8080
+);
+
+$server->run();
