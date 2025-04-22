83a29ecd96311f1c4933d3c0dd9400c80f4cc771
Noji Yudara <noji.yu123@gmail.com>
2025-04-22 16:28:53 +0530

    women category

---
+++ app/views/pages/inc/components/women-category.php
@@ -0,0 +1,42 @@
+<main class="new-collection">
+    <div class="topic">Select A Category</div><br>
+    <section class="collection-cards">
+        <!-- Tailored Shirts -->
+        <div class="card">
+            <div class="card-content">
+                <h4>custom</h4>
+                <h2>Tailored Dress</h2>
+                <a href="<?php echo URLROOT ?>/pages/mensCategories"> <button>Explore now</button></a>
+            </div>
+            <img src="<?php echo URLROOT; ?>/public/img/home/dress.jpg" alt="Tailored Shirts">
+        </div>
+
+        <!-- Tailored Suits -->
+        <div class="card">
+            <div class="card-content">
+                <h4>custom</h4>
+                <h2>Tailored Saree Jacket</h2>
+                <button>Explore now</button>
+            </div>
+            <img src="<?php echo URLROOT; ?>/public/img/home/saree_blouse1.jpg" alt="Tailored Saree Jacket">
+        </div>
+        
+        <!-- Tailored Pants -->
+        <div class="card">
+            <div class="card-content">
+                <h4>custom</h4>
+                <h2>Tailored Pants</h2>
+                <button>Explore now</button>
+            </div>
+            <img src="<?php echo URLROOT; ?>/public/img/home/pants.jpeg" alt="Tailored Pants">
+        </div>
+        <div class="card">
+            <div class="card-content">
+                <h4>custom</h4>
+                <h2>Tailored Pants</h2>
+                <button>Explore now</button>
+            </div>
+            <img src="<?php echo URLROOT; ?>/public/img/home/pants.jpeg" alt="Tailored Pants">
+        </div>
+    </section>
+</main>
