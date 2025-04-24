8845d32cd77a1720362a7b642625e168c0b73a68
Noji Yudara <noji.yu123@gmail.com>
2025-04-24 14:28:48 +0530

    change password

--- app/views/users/Customer/v_c_changepassword.php
+++ app/views/users/Customer/v_c_changepassword.php
@@ -2,6 +2,7 @@
 <?php require_once APPROOT . '/views/users/Customer/inc/sideBar.php'; ?>
 <?php require_once APPROOT . '/views/users/Customer/inc/topNavBar.php'; ?>
 <link rel='stylesheet' type='text/css' media='screen' href='<?php echo URLROOT; ?>/public/css/customer/profilebuttons_styles.css'>
+<script src="<?php echo URLROOT; ?>/public/js/customer/changepass-validations.js"></script>
 
 <div class="passcontainer">
     <main class="main-content">
@@ -10,28 +11,33 @@
 
             <?php flash('user_message'); ?>
 
-            <form class="change-form" action="<?php echo URLROOT; ?>/Users/changePassword" method="post">
-                
-                <!-- Current Password -->
-                <label for="current-password">Current Password</label>
-                <input type="password" id="current_password" name="current_password" value="<?php echo htmlspecialchars($data['current_password'] ?? ''); ?>">
-                <?php if (!empty($data['current_password_err'])) : ?>
-                    <span class="error"><?php echo $data['current_password_err']; ?></span>
-                <?php endif; ?>
+            <form id="changePasswordForm" class="change-form" action="<?php echo URLROOT; ?>/Customers/changePassword" method="post" onsubmit="return validateChangePasswordForm()">
+                <div class="form-group">
+                    <label for="current_password">Current Password</label>
+                    <input type="password" 
+                           id="current_password" 
+                           name="current_password" 
+                           placeholder="Enter current password" />
+                    <span id="current_password_err" class="error"><?php echo $data['current_password_err'] ?? ''; ?></span>
+                </div>
 
-                <!-- New Password -->
-                <label for="new-password">New Password</label>
-                <input type="password" id="new_password" name="new_password" value="<?php echo htmlspecialchars($data['new_password'] ?? ''); ?>">
-                <?php if (!empty($data['new_password_err'])) : ?>
-                    <span class="error"><?php echo $data['new_password_err']; ?></span>
-                <?php endif; ?>
+                <div class="form-group">
+                    <label for="new_password">New Password</label>
+                    <input type="password" 
+                           id="new_password" 
+                           name="new_password" 
+                           placeholder="Enter new password" />
+                    <span id="new_password_err" class="error"><?php echo $data['new_password_err'] ?? ''; ?></span>
+                </div>
 
-                <!-- Confirm Password -->
-                <label for="confirm-password">Confirm Password</label>
-                <input type="password" id="confirm_password" name="confirm_password" value="<?php echo htmlspecialchars($data['confirm_password'] ?? ''); ?>">
-                <?php if (!empty($data['confirm_password_err'])) : ?>
-                    <span class="error"><?php echo $data['confirm_password_err']; ?></span>
-                <?php endif; ?>
+                <div class="form-group">
+                    <label for="confirm_password">Confirm Password</label>
+                    <input type="password" 
+                           id="confirm_password" 
+                           name="confirm_password" 
+                           placeholder="Confirm new password" />
+                    <span id="confirm_password_err" class="error"><?php echo $data['confirm_password_err'] ?? ''; ?></span>
+                </div>
 
                 <button type="submit" class="btn-save">Save Changes</button>
             </form>
@@ -39,12 +45,10 @@
     </main>
 </div>
 
-<style>
+<!-- <style>
     .error {
         color: red;
         font-size: 14px;
     }
-</style>
+</style> -->
 
-</body>
-</html>
